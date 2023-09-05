6e4e9af6f16afa9b3025909356896bd036d68509
Vllaadd <vlladdiimiir@gmail.com>
2023-09-05 02:55:14 -0400

    setting up basic struture

---
+++ main.js
@@ -0,0 +1,29 @@
+var scene = new THREE.Scene();
+
+var camera = newTHREE.PersepctiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.SphereGeometry(1, 32, 32);
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var earth = new THREE.Mesh(geometry, material);
+scene.add(earth);
+
+var sunGeometry = new THREE.SphereGeometry(5, 32, 32);
+var sunMaterial = new THREE.MeshBasicMaterial({color: 0xffff00 });
+var sun = new THREE.Mesh(sunGeometry, sunMaterial);
+scene.add(sun);
+
+camera.position.z = 10;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    earth.rotation.y += 0.01;
+
+    renderer.render(Scene, camera);
+}
+
+animate();
