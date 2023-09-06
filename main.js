4e5c495be8b44f1ff40a604fac84a977994a98cb
Vllaadd <vlladdiimiir@gmail.com>
2023-09-06 03:12:43 -0400

    earth image applied

--- main.js
+++ main.js
@@ -6,13 +6,14 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.SphereGeometry(5, 32, 32);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var earth = new THREE.Mesh(geometry, material);
+var earthGeometry = new THREE.SphereGeometry(2, 32, 32);
+var earthTexture = new THREE.TextureLoader().load('earth.png');
+var earthMaterial = new THREE.MeshBasicMaterial({ map: earthTexture });
+var earth = new THREE.Mesh(earthGeometry, earthMaterial);
 earth.position.set(0, 0, 0);
 scene.add(earth);
 
-var sunGeometry = new THREE.SphereGeometry(2, 32, 32);
+var sunGeometry = new THREE.SphereGeometry(5, 32, 32);
 var sunMaterial = new THREE.MeshBasicMaterial({color: 0xffff00 });
 var sun = new THREE.Mesh(sunGeometry, sunMaterial);
 sun.position.set(10, 0, 0)
