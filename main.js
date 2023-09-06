506927b9c45e26c60aa2e98fab5daa2650a976f5
Vllaadd <vlladdiimiir@gmail.com>
2023-09-06 05:33:58 -0400

    earth orbiting around the sun

--- main.js
+++ main.js
@@ -6,26 +6,37 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var earthGeometry = new THREE.SphereGeometry(2, 32, 32);
+var sunGeometry = new THREE.SphereGeometry(10, 32, 32);
+var sunMaterial = new THREE.MeshBasicMaterial({
+    color: 0xffff00,
+    emissive: 0xFFD700 
+});
+var sun = new THREE.Mesh(sunGeometry, sunMaterial);
+sun.position.set(0, 0, 0)
+scene.add(sun);
+
+var earthGeometry = new THREE.SphereGeometry(4, 32, 32);
 var earthTexture = new THREE.TextureLoader().load('earth.png');
 var earthMaterial = new THREE.MeshBasicMaterial({ map: earthTexture });
 var earth = new THREE.Mesh(earthGeometry, earthMaterial);
-earth.position.set(0, 0, 0);
+
+var distanceFromSun = 40;
+earth.position.set(distanceFromSun, 0, 0);
 scene.add(earth);
 
-var sunGeometry = new THREE.SphereGeometry(5, 32, 32);
-var sunMaterial = new THREE.MeshBasicMaterial({color: 0xffff00 });
-var sun = new THREE.Mesh(sunGeometry, sunMaterial);
-sun.position.set(10, 0, 0)
-scene.add(sun);
-
-camera.position.z = 20;
+camera.position.z = 50;
 
 function animate() {
     requestAnimationFrame(animate);
 
-    earth.rotation.y += 0.01;
-    sun.rotation.y += 0.01;
+    // Update the Earth's position to simulate orbiting around the Sun
+    var earthOrbitSpeed = 0.001; // Adjust the speed as needed
+    var angle = Date.now() * earthOrbitSpeed;
+    earth.position.x = distanceFromSun * Math.cos(angle);
+    earth.position.z = distanceFromSun * Math.sin(angle);
+
+    earth.rotation.y += 0.01; // Rotate the Earth on its axis
+    sun.rotation.y += 0.01; // Rotate the Sun on its axis
 
     renderer.render(scene, camera);
 }
