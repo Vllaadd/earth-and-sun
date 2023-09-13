4ed88730101676ce46a52c91e738cadea052d3c9
Vllaadd <vlladdiimiir@gmail.com>
2023-09-13 02:53:52 -0400

    dragging still not working

--- main.js
+++ main.js
@@ -56,20 +56,20 @@ scene.add(moon);
 
 camera.position.z = 40;
 
-// INTERACTION 
-var earthRotating = true; // Flag to control Earth rotation
-
-// Add a click event listener to stop Earth rotation when clicked
-earth.addEventListener('click', function () {
-    earthRotating = !earthRotating; // Toggle rotation
-});
-
+// STOP THE ROTATION & ENABLE DRAGGING 
+var rotationEnabled = true;
 var mouseX = 0;
 var isDragging = false;
 
-// Add mousemove event listener to allow dragging of the Earth
+document.body.addEventListener('keydown', function (event) {
+    if(event.key === ' '){
+        rotationEnabled = !rotationEnabled;
+        isDragging = !isDragging;
+    }
+});
+
 renderer.domElement.addEventListener('mousemove', function (event) {
-    if (isDragging) {
+    if (!rotationEnabled && isDragging) {
         // Calculate the mouse movement
         var deltaX = (event.clientX - mouseX) / 100; // Adjust the sensitivity as needed
         // Update the Earth's position based on mouse movement
@@ -78,48 +78,45 @@ renderer.domElement.addEventListener('mousemove', function (event) {
     }
 });
 
-// Add mousedown event listener to start dragging
-renderer.domElement.addEventListener('mousedown', function (event) {
-    isDragging = true;
-    mouseX = event.clientX;
-});
-
 // Add mouseup event listener to stop dragging
 renderer.domElement.addEventListener('mouseup', function () {
     isDragging = false;
 });
 
+// Add keyup event listener to ensure dragging is stopped when space key is released
+document.body.addEventListener('keyup', function (event) {
+    if (event.key === ' ') {
+        isDragging = false;
+    }
+});
 
 function animate() {
     requestAnimationFrame(animate);
 
-    if (earthRotating) {
-        // Continue Earth's rotation
+    if (rotationEnabled) {
+        // Rotate the Earth
         earth.rotation.y += 0.01;
-    }
 
-    sun.rotation.y += 0.01;
-
-    var earthOrbitSpeed = 0.001;
-    var angle = Date.now() * earthOrbitSpeed;
-    earth.position.x = distanceFromSun * Math.cos(angle);
-    earth.position.z = distanceFromSun * Math.sin(angle);
-
-    var moonOrbitSpeed = 0.004;
-    var moonAngle = Date.now() * moonOrbitSpeed;
-    var moonX = distanceFromEarth * Math.cos(moonAngle);
-    var moonZ = distanceFromEarth * Math.sin(moonAngle);
-
-    moon.position.x = earth.position.x + moonX;
-    moon.position.z = earth.position.z + moonZ;
-
-    if (earth.rotation.y <= 0.01) {
+        // Rotate the Sun
         sun.rotation.y += 0.01;
+
+        // Calculate and set the Moon's position
+        var earthOrbitSpeed = 0.001;
+        var angle = Date.now() * earthOrbitSpeed;
+        earth.position.x = distanceFromSun * Math.cos(angle);
+        earth.position.z = distanceFromSun * Math.sin(angle);
+
+        var moonOrbitSpeed = 0.004;
+        var moonAngle = Date.now() * moonOrbitSpeed;
+        var moonX = distanceFromEarth * Math.cos(moonAngle);
+        var moonZ = distanceFromEarth * Math.sin(moonAngle);
+
+        moon.position.x = earth.position.x + moonX;
+        moon.position.z = earth.position.z + moonZ;
+
+        // Render the scene
+        renderer.render(scene, camera);
     }
-
-
-
-    renderer.render(scene, camera);
 }
 
 animate();
